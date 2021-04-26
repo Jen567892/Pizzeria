5d414a06f461e36ae9fa6755d11737437220558f
Jen567892 <82474362+Jen567892@users.noreply.github.com>
2021-04-25 20:28:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ menu_list_array.push(item);
 menu_list_array.sort();
 htmldata = "<section class='cards'>"
 for(i=0;i < menu_list_array.length ;i++){
-    htmldata=htmldata+'<div class="card">'+ '<img src="image/pizzaImg.png">' +menu_list_array[i] + '</div>';
+    htmldata=htmldata+'<div class="card">'+ '<img src="pizzaImg.png">' +menu_list_array[i] + '</div>';
 }
 htmldata=htmldata+"</section>"
     document.getElementById("display_addedmenu").innerHTML = htmldata;
